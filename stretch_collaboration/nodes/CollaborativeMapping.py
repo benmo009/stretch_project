25b2acad408025d49ee8b3b935e42f64b322b3ff
Ben <benmo009@gmail.com>
2021-04-25 15:27:36 -0400

    Added loading max height image data to param

--- stretch_collaboration/nodes/CollaborativeMapping.py
+++ stretch_collaboration/nodes/CollaborativeMapping.py
@@ -30,7 +30,7 @@ class FunmapNode(hm.HelloNode):
         # Create and perform a new full scan o f the environment using
         # the head
         head_scan = ma.HeadScan(voi_side_m=16.0)
-        head_scan.execute_full(node, fast_scan=fast_scan)
+        head_scan.execute_full(node)
 
         self.load_headscan_to_param(head_scan)
 
@@ -41,15 +41,14 @@ class FunmapNode(hm.HelloNode):
 
     
     def load_headscan_to_param(self, headscan):
-
+        # Load HeadScan data
         if "tolist" in dir(headscan.robot_ang_rad):
             robot_ang_rad = headscan.robot_ang_rad.tolist()
         else:
             robot_ang_rad = headscan.robot_ang_rad
-        data = {'max_height_image_base_filename' : max_height_image_base_filename,
-                'robot_xy_pix' : headscan.robot_xy_pix.tolist(),
+        hs_data = {'robot_xy_pix' : headscan.robot_xy_pix.tolist(),
                 'robot_ang_rad' : robot_ang_rad,
-                'timestamp' : {'secs':headscan.timestamp.secs, 'nsecs':self.timestamp.nsecs},
+                'timestamp' : {'secs':headscan.timestamp.secs, 'nsecs':headscan.timestamp.nsecs},
                 'base_link_to_image_mat' : headscan.base_link_to_image_mat.tolist(), 
                 'base_link_to_map_mat' : headscan.base_link_to_map_mat.tolist(), 
                 'image_to_map_mat' : headscan.image_to_map_mat.tolist(), 
@@ -57,7 +56,45 @@ class FunmapNode(hm.HelloNode):
                 'map_to_image_mat' : headscan.map_to_image_mat.tolist(), 
                 'map_to_base_mat' : headscan.map_to_base_mat.tolist()}
 
-        rospy.set_param("stretch_re1_2017/map", data)
+        rospy.set_param("stretch_re1_1027/HeadScan", hs_data)
+
+        # Load maxHeightImage data
+        rgb_im = self.max_height_im.rgb_image.tolist()
+        cam_depth_im = self.max_height_im.camera_depth_image.tolist()
+        im = self.max_height_im.image.tolist()
+
+        voi_data = self.max_height_im.voi.serialize()
+        voi_data['origin'] = voi_data['origin'].tolist()
+        voi_data['axes'] = voi_data['axes'].tolist()
+
+        if self.max_height_im.transform_original_to_corrected is not None:
+            transform_original_to_corrected = self.max_height_im.transform_original_to_corrected.tolist()
+        else:
+            transform_original_to_corrected = None
+
+        if self.max_height_im.transform_corrected_to_original is not None:
+            transform_corrected_to_original = self.max_height_im.transform_corrected_to_original.tolist()
+        else:
+            transform_corrected_to_original = None
+        
+        mha_data = {'image.dtype': str(self.max_height_im.image.dtype),
+                'image.shape': list(self.max_height_im.image.shape),
+                'np.max(image)': max_pix, 
+                'm_per_pix': self.max_height_im.m_per_pix,
+                'm_per_height_unit': self.max_height_im.m_per_height_unit,
+                'voi_data': voi_data,
+                'image_origin': self.max_height_im.image_origin.tolist(),
+                'transform_original_to_corrected': transform_original_to_corrected, 
+                'transform_corrected_to_original': transform_corrected_to_original
+        }
+
+        rospy.set_param("stretch_re1_1027/MaxHeightImage", mhi_data)
+        rospy.set_param("stretch_re1_1027/MaxHeightImage/image", im)
+        rospy.set_param("stretch_re1_1027/MaxHeightImage/rgb_image", rgb_im)
+        rospy.set_param("stretch_re1_1027/MaxHeightImage/camera_depth_image", cam_depth_im)
+
+
+    def get_headscan_from_param(self)
 
 if __name__ == "__main__":
     colab_map = FunmapNode()
