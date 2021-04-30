c77a26c00adfd59e502eddf1393ea675f895a17a
Ben <benmo009@gmail.com>
2021-04-30 16:00:27 -0400

    Added node for human localization

---
+++ stretch_trajectory_predict/nodes/HumanLocalizer.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import rospy
+from visualization_msgs.msg import MarkerArray, Marker
+from geometry_msgs.msg import Pose, Vector3
+
+class HumanLocalizer:
+    def __init__(self):
+        # Initialize ROS node
+        rospy.init_node("human_localizer")
+
+        self.rate = rospy.Rate(2)
+
+        # Initialize MarkerArray subscriber
+        self.face_sub = rospy.Subscriber("/faces/marker_array", MarkerArray, self.face_callback)
+        self.body_sub = rospy.Subscriber("/body_landmarks/marker_array", MarkerArray, self.body_callback)
+
+        self.marker_pub = rospy.Publisher("/localized_person", Marker, queue_size=0)
+
+        rospy.wait_for_message("/body_landmarks/marker_array", MarkerArray)
+        rospy.wait_for_message("/faces/marker_array", MarkerArray)
+
+    def face_callback(self, data):
+        if len(data.markers) > 0:
+            face_marker = data.markers[0]
+            self.face_pose = face_marker.pose
+
+        else:
+            rospy.loginfo("No face detected")
+            
+    
+    def body_callback(self, data):
+        # Take each point position and average them out
+        if len(data.markers) > 0:
+            points = data.markers[0].points 
+            avg_point = Vector3()
+            for point in points:
+                avg_point.x += point.x 
+                avg_point.y += point.y 
+                avg_point.z += point.z 
+
+            avg_point.x /= len(points)
+            avg_point.y /= len(points)
+            avg_point.z /= len(points)
+
+            marker = Marker()
+            marker.pose.orientation.w = 1
+            marker.header.frame_id = data.markers[0].header.frame_id
+            marker.pose.position=avg_point
+            marker.color.r = 1
+            marker.color.a = 1
+            marker.scale.x = 0.05
+            marker.scale.y = 0.05
+            marker.scale.z = 0.05
+            marker.type=1
+            
+            self.body_avg = avg_point
+
+            self.marker_pub.publish(marker)
+            rospy.loginfo(avg_point)
+
+        else:
+            rospy.loginfo("No body landmarks detected")
+            
+
+
+    def main(self):
+        while not rospy.is_shutdown():
+            rospy.loginfo("Face position - x: {:.3f} y: {:.3f} z: {:.3f}".format(
+                                                    self.face_pose.position.x, 
+                                                    self.face_pose.position.y, 
+                                                    self.face_pose.position.z))
+
+            rospy.loginfo("Body position - x: {:.3f} y: {:.3f} z: {:.3f}".format(
+                                                    self.body_avg.x, 
+                                                    self.body_avg.y, 
+                                                    self.body_avg.z))
+            rospy.loginfo(self.face_pose.position)
+            self.rate.sleep()
+        #rospy.spin()
+
+
+if __name__ == "__main__":
+    hl_node = HumanLocalizer()
+    hl_node.main()
+
